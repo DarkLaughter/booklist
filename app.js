fcd80b92dedb83b8f5cba34ca0cad185b369e9c5
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-03-18 21:15:35 -0400

    events and removing child via parent

--- app.js
+++ app.js
@@ -1,39 +1,61 @@
-const books = document.querySelectorAll('#book-list li .name')
-books.forEach(book => {
-    book.textContent += ' (book title)'
-});
+// const books = document.querySelectorAll('#book-list li .name')
+// books.forEach(book => {
+//     book.textContent += ' (book title)'
+// });
 
-const list = document.querySelector('#book-list')
+// const list = document.querySelector('#book-list')
 
-list.innerHTML += `<p>this how you add html</p>`
+// list.innerHTML += `<p>this how you add html</p>`
 
-const banner = document.querySelector('#page-banner')
-console.log(banner.nodeType)
-console.log(banner.nodeName)
-console.log(banner.hasChildNodes())
+// const banner = document.querySelector('#page-banner')
+// console.log(banner.nodeType)
+// console.log(banner.nodeName)
+// console.log(banner.hasChildNodes())
 
-//full clone of node, needs true passed into function call
-// entire node 
-const bannerClone = banner.cloneNode(true)
-console.log(bannerClone)
+// //full clone of node, needs true passed into function call
+// // entire node 
+// const bannerClone = banner.cloneNode(true)
+// console.log(bannerClone)
 
-// partial clone of banner node using false into function call
-// just div with page-banner id
-const bannerClone2 = banner.cloneNode(false)
-console.log(bannerClone2)
+// // partial clone of banner node using false into function call
+// // just div with page-banner id
+// const bannerClone2 = banner.cloneNode(false)
+// console.log(bannerClone2)
 
-//dom traversal
+// //dom traversal
 
-const booklst = document.querySelector('#book-list')
-// states parent node, mostly do the same thing
-console.log(booklst.parentNode)
-console.log(booklst.parentElement)
-// grabbing the parent of the parent
-console.log(booklst.parentElement.parentElement)
+// const booklst = document.querySelector('#book-list')
+// // states parent node, mostly do the same thing
+// console.log(booklst.parentNode)
+// console.log(booklst.parentElement)
+// // grabbing the parent of the parent
+// console.log(booklst.parentElement.parentElement)
 
-//referencing children nodes, grabs all nodes 
-console.log(booklst.childNodes)
+// //referencing children nodes, grabs all nodes 
+// console.log(booklst.childNodes)
 
-// grab child node elements
-console.log(booklst.children)
+// // grab child node elements
+// console.log(booklst.children)
 
+// ########
+
+
+//events 
+// const h2 = document.querySelector('#book-list h2')
+// h2.addEventListener('click', (e) => {
+//     console.log(e)
+//     console.log(e.target)
+// })
+
+const btns = document.querySelectorAll('#book-list .delete')
+
+btns.forEach((btn) => {
+
+    btn.addEventListener('click', (e) => {
+        const li = e.target.parentElement
+        li.parentNode.removeChild(li)
+
+    })
+
+
+})
