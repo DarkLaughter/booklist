0229e29e2e2c5e9f9ab93abb989baaf9c571cd44
Ramon Echeverria <ramon.l.echeverria@gmail.com>
2021-03-18 20:56:25 -0400

    node attributes and method calls

--- app.js
+++ app.js
@@ -6,3 +6,18 @@ books.forEach(book => {
 const list = document.querySelector('#book-list')
 
 list.innerHTML += `<p>this how you add html</p>`
+
+const banner = document.querySelector('#page-banner')
+console.log(banner.nodeType)
+console.log(banner.nodeName)
+console.log(banner.hasChildNodes())
+
+//full clone of node, needs true passed into function call
+// entire node 
+const bannerClone = banner.cloneNode(true)
+console.log(bannerClone)
+
+// partial clone of banner node using false into function call
+// just div with page-banner id
+const bannerClone2 = banner.cloneNode(false)
+console.log(bannerClone2)
